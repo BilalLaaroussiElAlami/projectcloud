3f69fb9b426f8289a73e77bc60c4381fae97539e
Bilal Lrs <bilal.laaroussi.el.alami@vub.be>
2024-01-06 14:23:55 +0100

    dockerfile for database

--- app.js
+++ app.js
@@ -3,10 +3,44 @@ const app = express();
 const port = process.env.PORT || 3000;
 const fs = require('fs');
 
-const images = [
-    'Alice', 'Bob', 'Charlie', 'David', 'Eva', 'Frank', 'Grace', 'Henry', 'Ivy', 'Jack',
-    'Kate', 'Liam', 'Mia', 'Noah', 'Olivia', 'Parker', 'Quinn', 'Riley', 'Sophia', 'Thomas',
-];
+
+function getRandomImage() {
+    try {
+        const files = fs.readdirSync("images_test");
+        const randomIndex = Math.floor(Math.random() * files.length);
+        const randomImg = files[randomIndex];
+        const randomimgPath = `images_test/${randomImg}`;
+        // console.log("random path", randomimgPath)
+        const data = fs.readFileSync(randomimgPath);
+        return data;
+    } catch (err) {
+        console.error('Error:', err);
+        return null;
+    }
+}
+
+function takePicture() { return getRandomImage() }
+
+//TODO: do actual call to head-count service
+function getHeadCount(picture) {
+    const count = 5 + Math.floor(Math.random() * 95)
+    return count
+}
+
+/*
+fs.readFile("images_test/1066405,1b8000ef60354f.jpg", (readErr, data) => {
+    if (readErr) {
+        console.error('Error reading the random image file:', readErr);
+        return;
+    }
+    console.log("hier,", data)
+})*/
+
+/*
+console.log("the result", takePicture())
+console.log("the result", getRandomImage())
+console.log("the result", getRandomImage())
+*/
 
 
 const delta_millis_upload = 1000 //every delta_millis_upload, an image is uploaded
@@ -19,77 +53,24 @@ const version = fs.readFileSync('version.txt', 'utf8');
 app.get('/', (req, res) => {
     res.send(`version ${version}, pod host name: ${podHostname}`)
 });
-
+ 
 app.listen(port, () => {
     console.log(`Camera Server running at http://localhost:${port}`);
 });
-
+ 
 */
 
 function main() {
     console.log(`CAMERA LIVE: version ${version}`);
-    /*setInterval(function () {
-        const randomImage = images[Math.floor(Math.random() * images.length)]
-        console.log(randomImage)
-    }, delta_millis_upload)*/
+    setInterval(function () {
+        console.log("ping")
+        const image = takePicture()
+        const headCount = getHeadCount(image)
+        const location = "antwerp"   //TODO how to manage location accross pods
+        console.log("image", image)
+        console.log("headcount", headCount)
+        console.log("location", location)
+    }, delta_millis_upload)
 }
+main();
 
-//-----------------CODE FOR ACCESSING DASHBOARD PODS------------------------
-const k8s = require('@kubernetes/client-node');
-const axios = require('axios');
-
-// Load Kubernetes config
-const kc = new k8s.KubeConfig();
-kc.loadFromDefault();
-
-// Create Kubernetes API client
-const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-
-// Define the namespace and label selector
-const namespace = 'default'; // Change this to your namespace if different
-const labelSelector = 'app=my-app,name=dashboard-deployment';
-
-// Function to make requests to pods
-async function makeRequestsToPods() {
-    try {
-        // Get pods with the specified label selector
-
-        const { body: podList } = await k8sApi.listNamespacedPod(namespace, undefined, undefined, undefined, undefined, labelSelector);
-
-        // Iterate through each pod and make requests
-        podList.items.forEach(pod => {
-            const podIP = pod.status.podIP;
-            const podPort = 30001; // Change this to your target port if different
-            console.log("======================================/n")
-            console.log("pod: ", pod)
-            console.log("======================================/n")
-            const submitDataEndpoint = `http://dashboard-service:3030/submit-data`   //  `http://${podIP}:${podPort}/submit-data`;
-
-            // Example request payload (adjust as needed)
-            const requestData = {
-                location: 'Sample Location',
-                time: 'Sample Time',
-                number: 12345,
-                // Add your image file here if required by your endpoint
-                // image: <your_image_data_here>
-            };
-
-            // Make an HTTP POST request to the submit-data endpoint
-            axios.post(submitDataEndpoint, requestData)
-                .then(response => {
-                    console.log(`Request to pod ${pod.metadata.name} successful:`, response.data);
-                })
-                .catch(error => {
-                    console.error(`Request to pod ${pod.metadata.name} failed:`, error.message);
-                });
-        });
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-// Call the function to make requests to pods
-makeRequestsToPods();
-
-
-main()
